d89f6f749396a9ac5dee4b10f50fc0b85adf0e2e
Erreur-404 <etienne-2.perron@polymtl.ca>
2022-02-17 12:58:44 -0500

    Changes, oh changes

--- Exercises/main.cpp
+++ Exercises/main.cpp
@@ -28,71 +28,80 @@ using namespace std;
 using namespace iter;
 
 
-void runArrayExample() {
-	Array<double, 3> foo;
-	foo[1] = 2.2; foo[2] = 4.4;
-	const auto& cfoo = foo;
-	print1(cout, &cfoo[0], foo.getSize());
-	cout << "\n";
-	Array bar = foo.toArray<int>();
-	print1(cout, &bar[0], bar.getSize());
-	cout << "\n";
-}
-
-void runPrintExample() {
-	int a[] = {4, 1, 3}; int b[] = {5, 6}; string c[] = {"b", "a"};
-	print1(cout, a, 3); print1(cout, b, 2); print1(cout, c, 2);
-	cout << "\n";
-
-	array<int, 2> arr = {3, 2};
-	vector<int> vec = {3, 2};
-	deque<int> deq = {1, 2, 3};
-	print2(cout, span<const int>(arr)); cout << "\n";
-	print2(cout, span<const int> (vec)); cout << "\n";
-	//print2(cout, span(arr)); cout << "\n";
-	//print2(cout, span(vec)); cout << "\n";
-	print3(cout, arr); cout << "\n";
-	print3(cout, vec); cout << "\n";
-	print3(cout, deq); cout << "\n";
-	//print3(cout, 42); cout << "\n"; // Erreur dans le for (pas capable d'itérer sur 42).
-}
-
-void runVectorExample() {
-	array<int, 3> values1 = {1, 2, 3};
-	array<int, 3> values2 = {10, 20, 30};
-	Vector foo(values1);
-	Vector bar(values2);
-	foo += bar + bar;
-	foo -= bar;
-	cout << foo << "\n";
-}
+//void runArrayExample() {
+//	Array<double, 3> foo;
+//	foo[1] = 2.2; foo[2] = 4.4;
+//	const auto& cfoo = foo;
+//	print1(cout, &cfoo[0], foo.getSize());
+//	cout << "\n";
+//	Array bar = foo.toArray<int>();
+//	print1(cout, &bar[0], bar.getSize());
+//	cout << "\n";
+//}
+//
+//void runPrintExample() {
+//	int a[] = {4, 1, 3}; int b[] = {5, 6}; string c[] = {"b", "a"};
+//	print1(cout, a, 3); print1(cout, b, 2); print1(cout, c, 2);
+//	cout << "\n";
+//
+//	array<int, 2> arr = {3, 2};
+//	vector<int> vec = {3, 2};
+//	deque<int> deq = {1, 2, 3};
+//	print2(cout, span<const int>(arr)); cout << "\n";
+//	print2(cout, span<const int> (vec)); cout << "\n";
+//	//print2(cout, span(arr)); cout << "\n";
+//	//print2(cout, span(vec)); cout << "\n";
+//	print3(cout, arr); cout << "\n";
+//	print3(cout, vec); cout << "\n";
+//	print3(cout, deq); cout << "\n";
+//	//print3(cout, 42); cout << "\n"; // Erreur dans le for (pas capable d'itérer sur 42).
+//}
+//
+//void runVectorExample() {
+//	array<int, 3> values1 = {1, 2, 3};
+//	array<int, 3> values2 = {10, 20, 30};
+//	Vector<int> foo(values1);
+//	Vector<int> bar(values2);
+//	foo += bar + bar;
+//	foo -= bar;
+//	cout << foo << "\n";
+//}
 
 void runLambdaExample() {
-	vector<int> foo = {1, 2, 3, 4, 5};
-	vector<int> bar = {10, 20, 30, 40, 50};
-	auto next1 = getNextFn(foo);
-	auto next2 = getNextFn(bar);
-	for (auto i : range(foo.size()))
-		cout << next1() << " ";
-	cout << "\n";
-	for (auto i : range(bar.size()))
-		cout << next2() << " ";
-	cout << "\n";
+	//vector<int> foo = {1, 2, 3, 4, 5};
+	//vector<int> bar = {10, 20, 30, 40, 50};
+	//auto next1 = getNextFn(foo);
+	//auto next2 = getNextFn(bar);
+	//for (auto i : range(foo.size()))
+	//	cout << next1() << " ";
+	//cout << "\n";
+	//for (auto i : range(bar.size()))
+	//	cout << next2() << " ";
+	//cout << "\n";
 
-	auto line = generateLine(10, 1);
-	for (auto&& x : {-1, 0, 1, 5, 10})
-		cout << line(x) << " ";
-	cout << "\n";
+	//auto line = generateLine(10, 1);
+	//for (auto&& x : {-1, 0, 1, 5, 10})
+	//	cout << line(x) << " ";
+	//cout << "\n";
+
+	int a = 2;
+	auto myLambda = [a]() {
+		a = 10;
+		return a * a;
+	};
+	cout << a << endl;
+	cout << myLambda() << endl;
+	cout << a << endl;
 }
 
 
 int main() {
-	runArrayExample();
+	//runArrayExample();
 	//cout << "\n\n\n";
 	//runPrintExample();
 	//cout << "\n\n\n";
 	//runVectorExample();
 	//cout << "\n\n\n";
-	//runLambdaExample();
+	runLambdaExample();
 }
 
